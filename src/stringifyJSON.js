8375d0012f024cccc400b58dcedc5f2f68bccdf5
Jeffrey Campecino <jeffrey.campecino@gmail.com>
2015-09-17 10:20:24 -0700

    Can stringify non-nested arrays and objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+    return 'null';
+  } else if (obj === undefined) {
+    return 'undefined';
+  } else if (typeof(obj) !== 'array' && typeof(obj !== 'object')) {
+    if (typeof(obj) === 'string') {
+      return '"' + obj + '"';
+    } else {
+      return obj.toString();
+    }
+  } else {
+    if (Array.isArray(obj)) {
+      var firstKey = Object.keys(obj)[0];
+      var firstProperty = obj[firstKey];
+
+      if (obj[firstKey] === undefined) {
+        return "]";
+      } else {
+        delete obj[firstKey];
+        if (arguments[1] === undefined) {
+          return ('["' + firstKey + '":' + firstProperty + stringifyJSON(obj, 1));
+        } else {
+          return (',"' + firstKey + '":' + firstProperty + stringifyJSON(obj, 1));
+        }
+      }
+    } else {
+      var firstKey = Object.keys(obj)[0];
+      var firstProperty = obj[firstKey];
+
+      if (obj[firstKey] === undefined) {
+        return "}";
+      } else {
+        delete obj[firstKey];
+        if (arguments[1] === undefined) {
+          return ('{"' + firstKey + '":' + firstProperty + stringifyJSON(obj, 1));
+        } else {
+          return (',"' + firstKey + '":' + firstProperty + stringifyJSON(obj, 1));
+        }
+      }
+    }
+  }
 };
