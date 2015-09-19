9f19f93a3fcaa20ef88600b937a3e9febdafce23
Jeffrey Campecino <jeffrey.campecino@gmail.com>
2015-09-19 12:06:29 -0700

    getElementsByClassName fixed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,50 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+  // array to be filled with the elements with the className
+  var elementsArray = [];
+
+  // function that checks if an element contains className
+  var checkClass = function(node, className) {
+    var bool = false;
+    each(node.classList, function(classItem) {
+      bool = bool || classItem === className;
+    });
+    return bool;
+  };
+
+  // 
+  var checkElement = function(node) {
+    if (checkClass(node, className)) {
+      elementsArray.push(node);
+    }
+  };
+
+  var getElements = function(node) {
+    checkElement(node);
+    each(node.childNodes, function(node) {
+      getElements(node);
+    });
+  };
+
+  getElements(document.body);
+
+  return elementsArray;
 };
+
+function each(collection, iterator) {
+  if (Array.isArray(collection)) {
+    for (var n = 0; n < collection.length; n++) {
+      iterator(collection[n]);
+    }
+  } else {
+    for (var key in collection) {
+      iterator(collection[key]);
+    }
+  }
+}
+
+
+
+
